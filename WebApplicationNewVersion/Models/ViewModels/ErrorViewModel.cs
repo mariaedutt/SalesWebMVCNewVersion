e3882856ff0f420fbffde6a88867eaef1f8e10b4
maria.brito <maria.brito@processor.com.br>
2024-01-04 21:16:14 -0300

    Primeira controller

---
+++ WebApplicationNewVersion/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationNewVersion.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
