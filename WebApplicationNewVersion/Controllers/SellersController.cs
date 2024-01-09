23d445372051099493108a74cad72e1bc9e412a5
maria.brito <maria.brito@processor.com.br>
2024-01-09 14:04:37 -0300

    SellerController

---
+++ WebApplicationNewVersion/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationNewVersion.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
