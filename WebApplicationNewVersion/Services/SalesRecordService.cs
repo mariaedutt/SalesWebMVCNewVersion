815fd3c3ebc77834e3ee160cbea4341554178847
maria.brito <maria.brito@processor.com.br>
2024-01-18 13:12:42 -0300

    Implementing grouping search

--- WebApplicationNewVersion/Services/SalesRecordService.cs
+++ WebApplicationNewVersion/Services/SalesRecordService.cs
@@ -30,5 +30,25 @@ namespace WebApplicationNewVersion.Services
                          .OrderByDescending(x => x.Date)
                          .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return result.Include(x => x.Seller)
+                         .Include(x => x.Seller.Department)
+                         .OrderByDescending(x => x.Date)
+                         .AsEnumerable()
+                         .GroupBy(x => x.Seller.Department)
+                         .ToList();
+        }
     }
 }
